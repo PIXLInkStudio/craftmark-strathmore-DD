687ddc197e9faa1bf81f31ad2a66d5709ae62179
Amra Narmandakh <anarmandakh@leaptodigital.com>
2025-06-18 16:39:23 +0200

    feat: (task-1210573408441076) update features copy

--- includes/components/features.php
+++ includes/components/features.php
@@ -3,67 +3,66 @@
         <div class="tab-pane fade in show active" id="luxury-features" role="tabpanel">
             <h2 class="royal-puerto-normal text-center occ-blue">Luxury Features</h2>
             <ul>
-                <li><span class="fw-500 occ-blue optima-normal">Exteriors</span>
-                    <ul>
-                        <li>Brick or Stone Accents on Front (per plan)</li>
-                        <li>James Hardie™ Hardie Panel Vertical Siding Board & Batten (per plan)</li>
-                        <li>James Hardie™ Hardie Plank Smooth Horizontal Lap Siding (Front, Sides, and Rear, per plan)</li>
-                        <li>Weather Resistant Exterior Trim</li>
-                        <li>Professional Landscaping Package (Includes Sod and Hydroseed - Lot Specific)</li>
-                    </ul>
-                </li>
-                <li><span class="fw-500 occ-blue optima-normal">Interiors</span>
-                    <ul>
-                        <li>3 Finished Levels (Includes Finished Rec Room)</li>
-                        <li>4 Bedrooms/ 4 Full Baths / 1 Half Bath</li>
-                        <li>Deck</li>
-                        <li>Gas Fireplace with Mantle Shelf and Slate Surround in Family Room</li>
-                        <li>9' Ceiling - 1st and 2nd Floor</li>
-                        <li>Cased Interior Windows</li>
-                        <li>5" Shaw Hickory Engineered Hardwood Flooring - Foyer, Powder Room, Kitchen, Eat-in Kitchen, Working Kitchen, Mud Room, Study</li>
-                        <li>Oak Stairs 1st to 2nd Floor (Painted Risers)</li>
-                        <li>5 1/4" Eased Edge Base Molding</li>
-                        <li>Shaw Designer Carpet in Selected Areas (per plan)</li>
-                        <li>Designer 2-Panel Square Smooth Doors with Schlage® </li>
-                        <li>Polished Chrome Hardware</li>
-                        <li>Multi-port Tech Wiring Package with RG-6 and CAT-6 (3 Locations)</li>
-                    </ul>
-                </li>
+                <li>Brick on Front (per plan)</li>
+                <li>James Hardie™ Hardie Panel (front, per plan)</li>
+                <li>James Hardie™ Hardie Plank Smooth Horizontal Lap Siding (Sides and Rear, per plan)</li>
+                <li>Weather Resistant Exterior Trim</li>
+                <li>Professional Landscaping Package (Includes Sod)</li>
+                <li>3 Finished Levels (Includes Finished Rec Room)</li>
+                <li>3 Bedrooms/ 3 Full Baths / 1 Half Bath</li>
+                <li>Pati o</li>
+                <li>10' Ceiling - 1st Floor 9' Ceiling - 2nd Floor and Opt. Atti c</li>
+                <li>Cased Interior Windows</li>
+                <li>6"+ Shaw® White Oak Engineered Hardwood Flooring - throughout 1st and 2nd Floor (Upper Hall and Primary Bedroom/Closet)</li>
+                <li>Shaw® Carpet in Rec Room and Bedrooms 2 & 3</li>
+                <li>Oak Stairs - All Stairs (Painted Risers)</li>
+                <li>5 1/4" Eased Edge Base Molding</li>
+                <li>Designer 1-Panel Square Smooth Doors with Schlage® Matt e Black Hardware</li>
+                <li>Kichler® Light Fixture Package</li>
+                <li>Multi -port Tech Wiring Package with RG-6 and CAT-6 (3 Locati ons)</li>
+                <li>(2) WiFi Access Points with (1) WiFi Router</li>
+                <li>Lift Master® Garage Door Opener with Two Remotes and Keypad</li>
+                <li>Sherwin-Williams® Low VOC Two-Tone Interior Paint Package</li>
+                <li>Recessed LED Lighti ng Package (per plan)</li>
             </ul>
         </div>
         <div class="tab-pane fade" id="gourmet-kitchen" role="tabpanel">
             <h2 class="optima-normal text-center occ-blue">Gourmet Kitchen</h2>
             <ul>
-                <li>Bosch® Stainless Steel Appliance Package</li>
-                <li>Energy Star® 21.6 cu. ft. French Door Refrigerator</li>
-                <li>Energy Star® Dishwasher</li>
-                <li>5-Burner Gas Cooktop with Chimney Hood</li>
-                <li>Wall Oven and Separate Microwave</li>
-                <li>Shaker Cabinets with Pullout Trash & Recycle Bin</li>
-                <li>White Subway Tile Backsplash</li>
+                <li>Bosch® Gourmet Stainless Steel Appliance Package
+                    <ul>
+                        <li>Energy Star® 21.6 cu. ft. French Door Refrigerator</li>
+                        <li>Energy Star® Dishwasher</li>
+                        <li>5-Heati ng Element Inducti on Cooktop with Chimney Hood</li>
+                        <li>Wall Oven and Separate Microwave</li>
+                    </ul>
+                </li>
+                <li>Full Overlay Maple Cabinets to Ceiling with Pullout Trash & Recycle Bins</li>
                 <li>Soft Close Dovetail Drawers and Soft Close Hinges</li>
-                <li>Quartz Countertops</li>
+                <li>Upgrade #2 Quartz Countertops with Tile Backsplash</li>
                 <li>Oversized Designer Kitchen Island</li>
-                <li>Pendant Lights per Standard Lighting Plan</li>
+                <li>Pendant Lights per Standard Lighti ng Plan</li>
                 <li>Single-Bowl Under Mount Stainless Steel Sink</li>
-                <li>Polished Chrome Delta® Pullout Faucet</li>
-                <li>Polished Chrome Hardware</li>
+                <li>Matt e Black Delta® Pullout Faucet</li>
+                <li>Matt e Black Cabinet Hardware</li>
+                <li>Garbage Disposal</li>
             </ul>
         </div>
         <div class="tab-pane fade" id="deluxe-baths" role="tabpanel">
             <h2 class="optima-normal text-center occ-blue">Deluxe Baths</h2>
             <ul>
-                <li><span class="fw-500 occ-blue optima-normal">Owner’s Suite Bath</span>
+                <li><span class="fw-500 occ-blue optima-normal">Primary Bath</span>
                     <ul>
                         <li>Raised Height Upgrade #1 Vanity</li>
                         <li>Upgrade #1 Quartz Vanity Top with Integral Sink</li>
                         <li>Dual Vanity Sinks</li>
                         <li>Designer 12" x 24" Floor Tile</li>
-                        <li>Designer 12" x 24" Wall Tiles</li>
-                        <li>Shower with Seat</li>
+                        <li>Designer 4" x 12" Wall Tiles</li>
+                        <li>Shower with Seat and Dual Shower Heads</li>
                         <li>Heavy Glass Shower Door</li>
-                        <li>Soaking Tub</li>
-                        <li>Polished Chrome Delta® Faucets and Bath Hardware</li>
+                        <li>Free-Standing Soaking Tub</li>
+                        <li>Matt e Black Delta® Faucets and Bath Accessories</li>
+                        <li>Designer Black Framed Mirrors</li>
                         <li>Elongated Low-Flow Toilet</li>
                     </ul>
                 </li>
@@ -72,15 +71,17 @@
                         <li>Raised Height Vanity</li>
                         <li>Quartz Vanity Top with Integral Sink</li>
                         <li>Designer 12" x 24" Floor Tile</li>
-                        <li>3" x 6" Gloss White Subway Wall Tile</li>
-                        <li>Polished Chrome Delta® Faucets</li>
+                        <li>Designer 4" x 12" White Wall Tile in Herringbone Patt ern</li>
+                        <li>Matt e Black Delta® Faucets and Bath Accessories</li>
+                        <li>Designer Black Framed Mirrors</li>
                         <li>Elongated Low-Flow Toilet</li>
                     </ul>
                 </li>
                 <li><span class="fw-500 occ-blue optima-normal">Powder Room</span>
                     <ul>
                         <li>Pedestal Sink</li>
-                        <li>Polished Chrome Delta® Faucet</li>
+                        <li>Matt e Black Delta® Faucet and Bath Accessories</li>
+                        <li>Designer Black Framed Mirror</li>
                         <li>Elongated Low-Flow Toilet</li>
                     </ul>
                 </li>
@@ -89,21 +90,31 @@
         <div class="tab-pane fade" id="energy-saving-features" role="tabpanel">
             <h2 class="optima-normal text-center occ-blue">Energy Saving Features</h2>
             <ul>
-                <li>ENERGY STAR® Version 3.1 Certified</li>
+                <li>ENERGY STAR® Version 3.1 Certi fi ed</li>
                 <li>Low-E/Argon Insulated Vinyl Windows</li>
                 <li>HERS® Rated Home (Independent Third Party)</li>
                 <li>Duct Blast and Blower Door Test on Every Home</li>
-                <li>Trane® High Efficiency Gas Furnace with Electric A/C</li>
-                <li>Rinnai Tankless Gas Hot Water Heater</li>
-                <li>R-49 Ceiling Insulation</li>
-                <li>R-21 Insulation on Exterior Walls (2x6 construction)</li>
-                <li>Spray Foam Insulation on Band Boards</li>
+                <li>Carrier® High Effi ciency Air Handler and Heat Pump</li>
+                <li>RUUD® Hybrid Electric Heat Pump Hot Water Heater</li>
+                <li>R-49 Ceiling Insulati on</li>
+                <li>R-21 Insulati on on Exterior Walls (2x6 constructi on)</li>
+                <li>Spray Foam Insulati on on Band Boards</li>
                 <li>LED Bulbs Throughout</li>
-                <li>Wi-Fi Programmable Thermostat</li>
-                <li>Air Infiltration Package</li>
-                <li>Mastic® Sealed Ductwork</li>
+                <li>WiFi Programmable Thermostat</li>
+                <li>Air Infi ltrati on Package</li>
+                <li>Masti c® Sealed Ductwork</li>
                 <li>Whole House Energy Seal Package</li>
-                <li>Water Efficient Shower Head in All Baths</li>
+                <li>Water Effi cient Shower Head in All Baths</li>
+            </ul>
+        </div>
+         <div class="tab-pane fade" id="commitment-to-quality" role="tabpanel">
+            <h2 class="optima-normal text-center occ-blue">Commitment to Quality</h2>
+            <ul>
+                <li>New Home Orientati on</li>
+                <li>Pre-Drywall Orientati on</li>
+                <li>Pre-Sett lement Orientati on</li>
+                <li>1-Year Final Inspecti on</li>
+                <li>10-Year QBW Limited Warranty</li>
             </ul>
         </div>
     </div>
@@ -112,7 +123,7 @@
 <div id="secondary-nav" class="sec-nav features-nav">
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link active" data-toggle="tab"  href="#luxury-features">Luxury Features</a>
+            <a class="nav-link active" data-toggle="tab" href="#luxury-features">Luxury Features</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" data-toggle="tab" href="#gourmet-kitchen">Gourmet Kitchen</a>
@@ -122,6 +133,9 @@
         </li>
         <li class="nav-item">
             <a class="nav-link" data-toggle="tab" href="#energy-saving-features">Energy Saving Features</a>
-        </li> 
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#commitment-to-quality">Commitment to Quality</a>
+        </li>
     </ul>
-</div> 
+</div>
